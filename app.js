200e35bd99ee413df3bdadd327df6284012fc48d
Pedr0124 <165020325+Pedr0124@users.noreply.github.com>
2024-04-01 11:30:04 -0300

    soma

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-alert("bem vindo ao jogo");
-
-let numeroSecreto = Math.floor(Math.random() * 5 + 1);
-let tentativas = 5;
-
-while (tentativas > 0) {
-    let numeroDigitado = parseInt(prompt("digite um numero de 1 a 5"));
-
-    if (numeroDigitado == numeroSecreto) {
-        alert(`voce acertou o numero ${numeroSecreto}`);
-        break;
-    }
-    else if (numeroDigitado > numeroSecreto) {
-        alert(`voce digitou ${numeroDigitado} que é um número maior`);
-    }
-    else {
-        alert(`voce digitou ${numeroDigitado} um numero menor`);
-    }
-    tentativas = tentativas - 1;
-
-
-    if (tentativas == 0) {
-        alert(`suas tentativas acabaram`)
-    }
-    else if (tentativas >= 2) {
-        alert(`voce ainda tem ${tentativas} tentativas`);
-    }
-    else {
-        alert(`voce tem ${tentativas} tentativa`)
-    }
+function somaNumeros(a,b){
+    let soma = a + b; 
+    alert(`a soma dos numeros ${a} e ${b} é ${soma}`);
 }
-let palavraTentativa
-if (tentativas > 0) {
-    tentativas = 4 - tentativas;
-    alert(`voce conseguiu em ${tentativas} tentativas`);
-}
+let primeiroNumero = parseInt(prompt(`Digite o primeiro número`));
+let segundoNumero = parseInt(prompt(`Digite o segundo número`));
+
+somaNumeros (primeiroNumero, segundoNumero);
---
+++ nomeIdade.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu nome")
+let idade = prompt("Digite sua idade")
+
+alert (`Nome: ${nome} 
+Idade: ${idade}`)
+
