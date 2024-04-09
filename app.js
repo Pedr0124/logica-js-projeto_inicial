439fe5f76481af9befd70f3fd477070f7a6f407a
Pedr0124 <165020325+Pedr0124@users.noreply.github.com>
2024-04-09 08:41:48 -0300

    2-maior-numero

--- app.js
+++
@@ -1,9 +0,0 @@
-let primeiroNumero = prompt(`digite o primeiro número`)
-let segundoNumero = prompt(`digite o segundo número`)
-
-if (primeiroNumero>segundoNumero){
-    alert(primeiroNumero)
-}
-else{
-    alert(segundoNumero)
-}
