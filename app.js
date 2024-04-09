d86d6e076e5058b3573fbe32b0af73e48535128b
Pedr0124 <165020325+Pedr0124@users.noreply.github.com>
2024-04-09 10:35:49 -0300

    3-soma-da-lista

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-function somaArray(arr){
-    let soma = 0;
+let palavra= prompt(`digite a palavra`)
+
+
+function Array(arr){
+  
     for(let i = 0; i < arr.length; i++){
         soma += arr[i];
     }
     return soma;
 }
 
-let lista = [2, 4, 6, 8, 10, 12];
-let soma = somaArray(lista);
-alert(soma);
+let lista = [];
 
 
-function maiorNumeroArray(arr){
-    let maior = arr[0];
+function palavraArray(arr){
+
     for(let i = 0; i < arr.length; i++){
         if(arr[i] > maior){
-            maior = arr[i];
+           
         }
     }
     return maior;
 }
 
 alert(maiorNumeroArray(lista));
+
+function mostrarInveso(arr){
+    let arrInversa = [];
+    for(let i = arr.length - 1; i >= 0; i--){
+        arrInversa.push(arr[i]);
+    }
+    return arrInversa;
+}
+
+alert(mostrarInveso(lista))
+
+if (arrInversa===palavra){
+    alert(`é um palídromo`)
+}
