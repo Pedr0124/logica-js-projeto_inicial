ac3d358e88cfe1fe41080c4a044eb18a0176f74f
Pedr0124 <165020325+Pedr0124@users.noreply.github.com>
2024-04-10 08:03:29 -0300

    8-vogais.js

---
+++ 8-vogais.js
@@ -0,0 +1,8 @@
+function isVogal(letra){
+    if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
+        alert (`é uma vogal`)
+    }
+    else{
+        alert(`não é vogal`)
+    }
+}
